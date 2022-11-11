73e4316e8e25abd4cd32bd22fad244c8d2dbb077
Guilherme Santos <gui.santos@ibm.com>
2022-11-11 15:10:50 -0300

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Guilherme Santos on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
